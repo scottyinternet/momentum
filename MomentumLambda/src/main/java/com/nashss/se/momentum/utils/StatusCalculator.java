8eb698289f4e6f2c2201645d7edb3049673c0d5f
Scott Griffin <griffin.scott88@gmail.com>
2023-09-05 16:18:01 -0500

    WIP added createMessage() comments

--- MomentumLambda/src/main/java/com/nashss/se/momentum/utils/StatusCalculator.java
+++ MomentumLambda/src/main/java/com/nashss/se/momentum/utils/StatusCalculator.java
@@ -30,8 +30,8 @@ public class StatusCalculator {
             eventSummaryList.add(new EventSummary(date, sum));
         }
 
-        Double middleSum = 0.0;
-        Double recentSum = eventSummaryList.get(0).getSummedMeasurement();
+        double middleSum = 0.0;
+        double recentSum = eventSummaryList.get(0).getSummedMeasurement();
 
         for (int i = 1; i < goal.getTimePeriod(); i++) {
             double measurement = eventSummaryList.get(i).getSummedMeasurement();
@@ -41,8 +41,8 @@ public class StatusCalculator {
             }
         }
 
-        Double todaysTotals = middleSum + eventSummaryList.get(0).getSummedMeasurement();
-        Double yesterdaysTotals = eventSummaryList.get(timePeriod+1).getSummedMeasurement();
+        double todaysTotals = middleSum + eventSummaryList.get(0).getSummedMeasurement();
+        double yesterdaysTotals = eventSummaryList.get(timePeriod+1).getSummedMeasurement();
 
         //calculate status
         // IN MOMENTUM
@@ -58,10 +58,20 @@ public class StatusCalculator {
         }
 
         //create status message
+        // variables
+        // :TARGET
+        // :TODAYSSUM
+        // :DIFF = TARGET - TODAYSSUM
 
+        // 1.  IN MOMENTUM - "Keep it up! You have a surplus of :DIFF :UNITS"
+        // 2.  IN MOMENTUM AND IN DANGER OF LOSING IT TOMORROW - "Hit (:DIFF minus last relevant day) tomorrow to stay in momentum"
+        // 3.  IN MOMENTUM AND IN DANGER OF LOSING IT TODAY - "Hit :DIFF today to stay in momentum"
+        // 4.  BUILDING MOMENTUM "Keep it up, :DIFF more minutes until you are In Momentum!"
+        // 5.  LOSING MOMENTUM "You haven't had an entry in the last :TIMEPERIOD / 2 days"
+        // 6.  NO MOMENTUM "The best time to plan a tree was "TIMEPERIOD days ago... the second best time is today" //(jk on this message, but yeah something like that)
 
+        // perhaps we want to add two IN_MOMENTUM statuses to the enum to represent 2. and 3... the string can still be "In Momentum"
 
         return new Status(statusEnum, "a very helpful status message", eventSummaryList);
-
     }
 }
