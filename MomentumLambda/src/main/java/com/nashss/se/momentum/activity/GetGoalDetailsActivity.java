b1dff4d11332cde3d262a3da79e873ca9f9f6c50
Scott Griffin <griffin.scott88@gmail.com>
2023-09-12 10:31:57 -0500

    dealth with some merge conflicts

--- MomentumLambda/src/main/java/com/nashss/se/momentum/activity/GetGoalDetailsActivity.java
+++ MomentumLambda/src/main/java/com/nashss/se/momentum/activity/GetGoalDetailsActivity.java
@@ -44,7 +44,7 @@ public class GetGoalDetailsActivity {
         String goalSummaryMessage = "Target: " + goal.getTarget() + " " + goal.getUnit() + " within a rolling " + goal.getTimePeriod() + " day period.";
 
         Status status = StatusCalculator.calculateStatus(goal, eventModels);
-        GoalDetailsModel goalDetailsModel = new GoalDetailsModel(status, eventModels, goalSummaryMessage, requestedGoalName);
+        GoalDetailsModel goalDetailsModel = new GoalDetailsModel(status, eventModels, goalSummaryMessage, requestedGoalName, goal.getUnit());
 
         return GetGoalDetailsResult.builder()
                 .withGoalDetailModel(goalDetailsModel)
