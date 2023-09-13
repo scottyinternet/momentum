6aaf78d5a6e1de5e0de78976db329fdcb1c895b9
Ben Bagwell <129309155+benbagwell@users.noreply.github.com>
2023-09-13 12:27:34 -0400

    Update GoalDao.java
    Corrected a merge issue

--- MomentumLambda/src/main/java/com/nashss/se/momentum/dynamodb/GoalDao.java
+++ MomentumLambda/src/main/java/com/nashss/se/momentum/dynamodb/GoalDao.java
@@ -1,11 +1,13 @@
 package com.nashss.se.momentum.dynamodb;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
 import com.nashss.se.momentum.dynamodb.models.Goal;
 import com.nashss.se.momentum.metrics.MetricsPublisher;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.List;
 
 @Singleton
 public class GoalDao {
@@ -27,6 +29,17 @@ public class GoalDao {
     public Goal getGoal(String userId, String goalName) {
         return this.dynamoDBMapper.load(Goal.class, userId, goalName);
     }
+
+    public List<Goal> getGoals(String userId) {
+        Goal goalPartition = new Goal();
+        goalPartition.setUserId(userId);
+
+        DynamoDBQueryExpression<Goal> dynamoDBQueryExpression = new DynamoDBQueryExpression<Goal>()
+                .withHashKeyValues(goalPartition);
+
+        return this.dynamoDBMapper.query(Goal.class, dynamoDBQueryExpression);
+    }
+    
     public Goal updateGoal(Goal goal){
         this.dynamoDBMapper.save(goal);
         return goal;
